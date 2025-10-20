732066d611cbf9ee2f2f83eeead83fe903cd4a00
Ahmed MESRI <ahmed.mesri@etu.unilim.fr>
2025-10-20 11:16:28 +0200

    liens vers accueil et conexion

--- src/Controllers/homeController.php
+++ src/Controllers/homeController.php
@@ -9,11 +9,11 @@ class homeController {
     }
 
     function creerQuestionnaire(){
-
+        require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'connexion'.DIRECTORY_SEPARATOR.'home.php');
     }
  
     function index(){
         //lien vers la vue
-        require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'home.php');
+        require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'Accueil'.DIRECTORY_SEPARATOR.'home.php');
     }
 }
