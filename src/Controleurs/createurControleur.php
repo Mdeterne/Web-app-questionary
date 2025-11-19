6dc17b19f020a03899ad7eccfaeb2e16ba35dd03
AhmedTidjaniMesri <ahmed.mesri@etu.unilim.fr>
2025-11-19 10:38:09 +0100

    ajout analyse (pas fini)

--- src/Controleurs/createurControleur.php
+++ src/Controleurs/createurControleur.php
@@ -2,11 +2,11 @@
 class createurControleur{
 
     function index(){
-
+        require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'createur'.DIRECTORY_SEPARATOR.'.php');
     }
 
     function espacePerso(){
-
+        
     }
 
     function historique(){
